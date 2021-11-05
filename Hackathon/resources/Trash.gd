fd56e18d76254d93c21f0f4a02899b3c9557ffaa
Simon Damberg <damberg.simon00@gmail.com>
2021-11-05 20:15:04 +0100

    fixed merge

--- Hackathon/resources/Trash.gd
+++ Hackathon/resources/Trash.gd
@@ -2,17 +2,12 @@ extends Node
 
 export var value = 1
 
-<<<<<<< HEAD
 func _test_func(trash_value):
 	print("hit trash lol")
-=======
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
->>>>>>> 77e0c661e7d3335a5e0df7f4831198d58808147c
